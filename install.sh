c00a4a3f517bd5b5a8bd62bb0b8144906355c3ad
Sora-fox <106887689+Sora-Fox@users.noreply.github.com>
2025-05-16 22:51:29 +0300

    Bash: Remove config

--- install.sh
+++ install.sh
@@ -5,14 +5,12 @@ if ! command -v stow &> /dev/null; then
     exit 1
 fi
 
+stow -t $HOME zsh
 stow -t $HOME alacritty
 stow -t $HOME qutebrowser
 
 stow -t $HOME dunst
 stow -t $HOME xorg
-
-stow -t $HOME bash
-stow -t $HOME zsh
 
 stow -t $HOME emacs
 stow -t $HOME vim
