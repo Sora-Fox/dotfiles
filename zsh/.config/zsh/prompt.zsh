95a270e2cdcca5f86e5d691cd96072dfb44cfb46
xd <dhhugtu2@gmail.com>
2025-06-21 18:05:54 +0300

    Zsh: simplify prompt

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -2,28 +2,9 @@
 
 custom_prompt()
 {
-    local WDIR_COL="%F{#88C0D0}"
-    local GITB_COL="%F{#A3BE8C}"
-    local GITM_COL="%F{#EBCB8B}"
-    local GITC_COL="%F{#EA714F}"
     local SYMB_COL="%F{#D8DEE9}"
     local NULL_COL="%f"
-    local SEP="${SYMB_COL} :: ${NULL_COL}"
-
-    PROMPT=${WDIR_COL}%~${NULL_COL}
-    local branch=$(git branch --show-current 2>/dev/null)
-    if [[ -n "$branch" ]]; then
-        PROMPT+=${SEP}
-        if git status --porcelain 2>/dev/null | grep -qE "^ M|^M"; then
-            PROMPT+=${GITM_COL}$branch\*${NULL_COL}
-        else
-            PROMPT+=${GITB_COL}$branch${NULL_COL}
-        fi
-    elif git rev-parse --git-dir &>/dev/null; then
-        PROMPT+=${SEP}${GITC_COl}conflict${NULL_COL}
-    fi
-    PROMPT+=$'\n'
-    PROMPT+="${SYMB_COL}>${NULL_COL} "
+    PROMPT="${SYMB_COL}>${NULL_COL} "
 }
 
 autoload -Uz add-zsh-hook
