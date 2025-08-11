6ed486d7d5962d10c0b7d64ef5cbe9b766df387d
xd <dhhugtu2@gmail.com>
2025-08-12 00:12:34 +0300

    Xorg: Add commands checks

--- xorg/.config/xorg/xinitrc.sh
+++ xorg/.config/xorg/xinitrc.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # ~/.config/xorg/xinitrc.sh
 
-setxkbmap -layout us,ru -option grp:win_space_toggle -option ctrl:nocaps 
-redshift -O 5000
-
-dunst &
-flameshot &
-diodon &
-picom &
-${HOME}/.config/xorg/status.sh &
-
+command -v setxkbmap &>/dev/null && setxkbmap -layout us,ru -option grp:win_space_toggle -option ctrl:nocaps
+command -v redshift &>/dev/null && redshift -O 5000
+command -v dunst &>/dev/null && dunst &
+command -v flameshot &>/dev/null && flameshot &
+command -v diodon &>/dev/null && diodon &
+command -v picom &>/dev/null && picom &
+[ -x "${HOME}/.config/xorg/status.sh" ] && "${HOME}/.config/xorg/status.sh" &
 exec dwm
 
