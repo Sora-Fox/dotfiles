c3056e031cad98e4bfba01f17bc9286fa0116606
Fox <dhhugtu2@gmail.com>
2025-08-15 10:04:43 +0300

    Xorg: Update picom & Add gentoo-pipewire-launcher

--- xorg/.config/xorg/xinitrc.sh
+++ xorg/.config/xorg/xinitrc.sh
@@ -3,10 +3,11 @@
 
 command -v setxkbmap &>/dev/null && setxkbmap -layout us,ru -option grp:win_space_toggle -option ctrl:nocaps
 command -v redshift &>/dev/null && redshift -O 5000
+command -v picom &>/dev/null && picom --daemon --vsync --backend glx
 command -v dunst &>/dev/null && dunst &
 command -v flameshot &>/dev/null && flameshot &
 command -v diodon &>/dev/null && diodon &
-command -v picom &>/dev/null && picom &
+command -v gentoo-pipewire-launcher &>/dev/null && gentoo-pipewire-launcher &
 [ -x "${HOME}/.config/xorg/status.sh" ] && "${HOME}/.config/xorg/status.sh" &
-exec dwm
+exec dbus-launch --sh-syntax --exit-with-session dwm
 
