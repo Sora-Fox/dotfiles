af73d01cee7ee32a0ac6bd072dd7d7f73e454a52
Sora-fox <106887689+Sora-Fox@users.noreply.github.com>
2025-11-29 22:20:16 +0300

    Xorg: inc status interval & brightness

--- xorg/.config/xorg/xinitrc.sh
+++ xorg/.config/xorg/xinitrc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # ~/.config/xorg/xinitrc.sh
 
+brightnessctl set 5%
 command -v setxkbmap &>/dev/null && setxkbmap -layout us,ru -option grp:win_space_toggle -option ctrl:nocaps
 command -v redshift &>/dev/null && redshift -O 5000
 command -v picom &>/dev/null && picom --daemon --vsync --backend glx
